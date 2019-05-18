bcca7141b7811d45369747c1b0b61ef6a87101ae
Ajay Pethani <ajaypethani@Ajays-MacBook-Pro.local>
2019-05-18 14:36:59 +0100

    problem 36

---
+++ p36.py
@@ -0,0 +1,24 @@
+import time
+
+start_time = time.time()
+
+def binary(n):
+    return int(bin(n)[2:])
+
+def is_palindrome(n):
+    n = str(n)
+    if n == n[::-1]:
+        return True
+    else:
+        return False
+
+total = 0
+for i in range(1, 1000000):
+    if is_palindrome(i):
+        if is_palindrome(binary(i)):
+            total += i
+print(total)
+
+end_time = time.time()
+
+print("Time taken: {} ms".format(round((end_time - start_time)*1000, 3)))
