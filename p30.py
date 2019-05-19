6a8826d8f6ac15e14df237ddb7991c8f6b8c3ead
Ajay Pethani <ajaypethani97@gmail.com>
2019-05-19 12:38:18 +0100

    no longer using keyword as variable

--- p30.py
+++ p30.py
@@ -5,11 +5,11 @@ start_time = time.time()
 powers = []
 
 for i in range(2,354294):
-        sum = 0
+        total = 0
         for j in str(i):
-                sum += int(j) ** 5
+                total += int(j) ** 5
 
-        if sum == i:
+        if total == i:
                 powers.append(i)
 
 print(sum(powers))
