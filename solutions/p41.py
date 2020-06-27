f1148661ef01ab4534b226260a35200aab8af429
Ajay Pethani <ajaypethani97@gmail.com>
2020-06-27 11:49:35 +0100

    correct function name

--- solutions/p41.py
+++ solutions/p41.py
@@ -5,14 +5,14 @@ from util import is_prime
 
 start_time = time.time()
 
-def n_digit_odd_pandigital_numbers(n):
+def n_digit_pandigital_numbers(n):
     perms = itertools.permutations(string.digits[1:n+1][::-1])
     perms = [''.join(perm) for perm in perms]
     perms = list(map(int, perms))
     return perms
 
 for n in range(9, 1, -1):
-    pandigital_numbers = n_digit_odd_pandigital_numbers(n)
+    pandigital_numbers = n_digit_pandigital_numbers(n)
     odd_pandigital_numbers = list(filter(lambda x: x%2, pandigital_numbers))
     for number in odd_pandigital_numbers:
         if is_prime(number):
